055b9f8cb888fbd56522cc6db5d633a4ba9c759e
EDWIN LUDE <mwauraedwin22@gmail.com>
2026-06-04 22:21:30 +0300

    added ui

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <h2>Movie Watchlist</h2>
+            <ul>
+                <li>Home</li>
+                <li>Search </li>
+                <li>Watchlist</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="home">
+            <h1>🎬Welcome to Movie Watchlist</h1>
+            <p>Search movies, save them to your watchlist, and track what you've watched</p>
+        </div>
+    );
+}
+
+export default Home;
