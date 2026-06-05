5a1315f5da1d88fba2cca61f2e6fa029277d5a7b
Zakaria Abdi <zakariaabdisheikh004@gmail.com>
2026-06-06 00:30:51 +0300

    Movie-list updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import Footer from "./components/footer";
+import MovieList from "./components/MovieList";
 import "./App.css";
 
 function App() {
+    const localMovies = [
+    { id: 1, title: 'Inception', year: '2010', rating: '8.8', status: 'Watched' },
+    { id: 2, title: 'Interstellar', year: '2014', rating: '8.6', status: 'Plan to Watch' },
+    { id: 3, title: 'The Dark Knight', year: '2008', rating: '9.0', status: 'Watched' }
+  ];
     return (
         <>
             <Navbar />
             <Home />
+            <MovieList moviesList={localMovies} />
             <Footer />
         </>
     );
