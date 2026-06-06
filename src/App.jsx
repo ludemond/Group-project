746d3ce66302f235361e6633b585dea079ea0d26
Zakaria Abdi <zakariaabdisheikh004@gmail.com>
2026-06-06 22:03:11 +0300

    Usestate updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import Footer from "./components/footer";
@@ -5,11 +6,11 @@ import MovieList from "./components/MovieList";
 import "./App.css";
 
 function App() {
-    const localMovies = [
-    { id: 1, title: 'Inception', year: '2010', rating: '8.8', status: 'Watched' },
-    { id: 2, title: 'Interstellar', year: '2014', rating: '8.6', status: 'Plan to Watch' },
-    { id: 3, title: 'The Dark Knight', year: '2008', rating: '9.0', status: 'Watched' }
-  ];
+    const [localMovies, setLocalMovies] = useState([
+        { id: 1, title: 'Inception', year: '2010', rating: '8.8', status: 'Watched' },
+        { id: 2, title: 'Interstellar', year: '2014', rating: '8.6', status: 'Plan to Watch' },
+        { id: 3, title: 'The Dark Knight', year: '2008', rating: '9.0', status: 'Watched' }
+    ]);
     return (
         <>
             <Navbar />
